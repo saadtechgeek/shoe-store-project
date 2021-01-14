86dddd82adfa47c88d1ee83233cdc75925c1d909
saadtechgeek <saad.techgeek@gmail.com>
2021-01-14 05:46:34 +0500

    added navigation option with material ui

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,123 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import HomeIcon from '@material-ui/icons/Home';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-function Navbar(){
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-  
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
+export default function Navbar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/product">Product</Link>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon onClick={handleClick}/>
+            <StyledMenu
+        id="customized-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <StyledMenuItem component={Link} to="/">
+          <ListItemIcon>
+            <HomeIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Home" />
+        </StyledMenuItem>
+        <StyledMenuItem component={Link} to="/product">
+          <ListItemIcon >
+            <ShoppingCartIcon fontSize="small" />
+          </ListItemIcon>
+          <ListItemText primary="Product" />
+        </StyledMenuItem>
+      </StyledMenu>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Nike Store
+          </Typography>
+          { <Button color="inherit">Login</Button> }
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
 
-export default Navbar;
+
+// function Navbar(){
+
+  
+//   return (
+//     <div>
+//       <Link to="/">Home</Link>
+//       <Link to="/about">About</Link>
+//       <Link to="/product">Product</Link>
+//     </div>
+//   );
+// }
+
+// export default Navbar;
